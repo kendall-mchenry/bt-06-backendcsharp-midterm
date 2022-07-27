5a80f598a2ab5389f79db4e9d69b9eae80c84b43
Kendall McHenry <kendallexplores@gmail.com>
2022-07-27 16:14:24 -0500

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace midterm_project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
